a07e4f59d2e17a330f775273ce4efd0508618b24
italo V. <italovinicius.0105@gmail.com>
2026-04-24 19:25:24 +0000

    inicio

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+    const express = require('express');
+    const http = require('http');
+    const {SERVER} = require(Socket.io);
+    const cors = require('cors');
+    const path = require('path');
+
+    const app = express();
+    const server = http.createServer(app);
+    const io = new Server(server, {
+        cors: { origin: '*', methods: ['GET', 'POST']}
+        });
+
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    const PORT = process.env.PORT || 3000;
+    server.listen(PORT, () => {
+        console.log('Servidor rodando na porta ${PORT}');
+        });
+
+    module.exports = {io};
